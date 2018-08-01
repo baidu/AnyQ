25a0a31788a2d571eee8dbc88b6e9068e27233b7
zhanghan1992 <zhanghan17@baidu.com>
2018-08-01 22:57:17 +0800

    glog without gflag

--- cmake/external/glog.cmake
+++ cmake/external/glog.cmake
@@ -8,13 +8,22 @@ set(GLOG_INSTALL_DIR    ${THIRD_PARTY_PATH})
 ExternalProject_Add(
     ${GLOG_PROJECT}
     ${EXTERNAL_PROJECT_LOG_ARGS}
-    DOWNLOAD_DIR          ${GLOG_DOWNLOAD_DIR}
-    DOWNLOAD_COMMAND      wget --no-check-certificate https://codeload.github.com/google/glog/zip/v0.3.5 -c -q -O glog-0.3.5.zip
-                          && unzip glog-0.3.5.zip
-    DOWNLOAD_NO_PROGRESS  1
-    PREFIX                ${GLOG_SOURCES_DIR}
-    CONFIGURE_COMMAND     ""
-    BUILD_COMMAND         ""
-    INSTALL_COMMAND       cd ${GLOG_DOWNLOAD_DIR}/glog-0.3.5/ && ./configure --prefix=${GLOG_INSTALL_DIR} && make && make install
-    UPDATE_COMMAND        ""
+    GIT_REPOSITORY  "https://github.com/google/glog.git"
+    GIT_TAG         "v0.3.5"
+    PREFIX          ${GLOG_SOURCES_DIR}
+    UPDATE_COMMAND  ""
+    CMAKE_ARGS      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                    -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+                    -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+                    -DCMAKE_PREFIX_PATH=${GLOG_SOURCES_DIR}
+                    -DCMAKE_INSTALL_PREFIX=${GLOG_INSTALL_DIR}
+                    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+                    -DWITH_GFLAGS=OFF
+                    ${EXTERNAL_OPTIONAL_ARGS}
+    CMAKE_CACHE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${GLOG_INSTALL_DIR}
+                     -DBUILD_SHARED_LIBS:BOOL=ON 
+                     -DBUILD_STATIC_LIBS:BOOL=ON
+                     -DWITH_GFLAGS:BOOL=OFF 
+                     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
 )
