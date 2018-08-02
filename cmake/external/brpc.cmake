1505670b3511ba390142d0dd9796d47e02d458b6
zhanghan1992 <zhanghan17@baidu.com>
2018-08-02 11:50:22 +0800

    epoxrt protoc to PATH

--- cmake/external/brpc.cmake
+++ cmake/external/brpc.cmake
@@ -3,6 +3,7 @@ INCLUDE(ExternalProject)
 SET(BRPC_SOURCES_DIR ${THIRD_PARTY_PATH}/brpc)
 SET(BRPC_INSTALL_DIR ${THIRD_PARTY_PATH}/)
 SET(BRPC_DOWNLOAD_DIR  ${BRPC_SOURCES_DIR}/src/)
+#set(ENV{PATH} ${THIRD_PARTY_PATH}/bin:$ENV{PATH} )
 ExternalProject_Add(
     extern_brpc
     ${EXTERNAL_PROJECT_LOG_ARGS}
@@ -14,7 +15,7 @@ ExternalProject_Add(
     BUILD_COMMAND          ""
     UPDATE_COMMAND         ""
     CONFIGURE_COMMAND      ""
-    INSTALL_COMMAND        cd ${BRPC_DOWNLOAD_DIR}/brpc/ 
+    INSTALL_COMMAND        cd ${BRPC_DOWNLOAD_DIR}/brpc/ && export PATH=${THIRD_PARTY_PATH}/bin:$ENV{PATH}
                            && sh config_brpc.sh --headers=${BRPC_INSTALL_DIR}/include --libs=${BRPC_INSTALL_DIR}/lib --with-glog
                            && make
     BUILD_IN_SOURCE 1
