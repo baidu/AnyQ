84b8fbd440eebc905401bcb749e586a37bb8d5f6
yinweichong <yinweichong@baidu.com>
2018-08-08 17:44:56 +0800

    Update anyq_deps.sh
    python2.7 jdk>=1.8

--- tools/anyq_deps.sh
+++ tools/anyq_deps.sh
@@ -8,6 +8,7 @@ fi
 # 下载定制版本solr
 # 在solr-4.10.3版本基础上加入了百度开源词法分析作为中文分词插件
 # 用户可以使用此版本，或使用自己定制的solr版本
+# 依赖python2.7(with json) jdk1.8以上
 if [ ! -d solr-4.10.3-anyq ]; then
     wget --no-check-certificate http://anyq.bj.bcebos.com/solr-4.10.3-anyq.tar.gz
     tar xzf solr-4.10.3-anyq.tar.gz
