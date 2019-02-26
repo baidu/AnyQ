cd41a93303b09483bf5f5b1a05be6acf505d35e3
jchl <luo_smart@163.com>
2019-02-26 14:19:01 +0800

    Update run_infer.sh

--- tools/simnet/train/tf/run_infer.sh
+++ tools/simnet/train/tf/run_infer.sh
@@ -3,7 +3,7 @@ set -u # set -o nounset
 set -o pipefail 
 
 in_task_type='predict'
-in_task_conf='./examples/cnn-pointwise.json'
+in_task_conf='./examples/lstm-pointwise.json'
 python tf_simnet.py \
 		   --task $in_task_type \
 		   --task_conf $in_task_conf
